df9a4c6fe762c0357676aec2c0cf5cc7417c29e5
fupeng <fupenglxy@gmail.com>
2022-09-28 13:22:29 +0800

    chore: ci

--- packages/react-cli-service/src/utils/getNpmClient.ts
+++ packages/react-cli-service/src/utils/getNpmClient.ts
@@ -1,23 +1,16 @@
-import path from 'node:path'
-import fs from 'fs-extra'
 import { findUpSync } from 'find-up'
 
 const npmClientLockMap = {
   npm: ['package-lock.json', 'npm-shrinkwrap.json'],
-  pnpm: ['yarn.lock'],
-  yarn: ['pnpm-lock.yaml']
+  yarn: ['yarn.lock'],
+  pnpm: ['pnpm-lock.yaml']
 }
 
 export type NpmClientType = keyof typeof npmClientLockMap
 
 function getNpmClient(context: string): NpmClientType {
   for (const [key, list] of Object.entries(npmClientLockMap)) {
-    if (list.some((f) => fs.existsSync(path.join(context, f)))) {
-      return key as NpmClientType
-    }
-  }
-  for (const [key, list] of Object.entries(npmClientLockMap)) {
-    if (list.some((f) => findUpSync(path.join(context, f)) != null)) {
+    if (findUpSync(list, { cwd: context })) {
       return key as NpmClientType
     }
   }
